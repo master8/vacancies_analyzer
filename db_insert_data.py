85ea0336ae72ff22d0a15c2ec50e3f53906c3766
master8 <master8.michael@gmail.com>
2018-12-10 22:56:16 +0500

    add scripts and vacancies

--- db_insert_data.py
+++ db_insert_data.py
@@ -2,6 +2,9 @@ import pandas as pd
 from app import db
 from models import Region
 from models import Profstandard
+from models import Vacancy
+from models import ClassificatedVacancy
+from datetime import datetime
 
 # Накатывать на чистую базу
 
@@ -21,5 +24,34 @@ profstandards['is_support'] = profstandards['is_support'].apply(lambda v: v == '
 for index, pr in profstandards.iterrows():
     value = Profstandard(id=pr['id'], name=pr['name'], code=pr['code'], is_support=pr['is_support'])
     db.session.add(value)
+
+db.session.commit()
+
+
+vacancies = pd.read_csv('data/vacancies.csv')
+
+for index, vacancy in vacancies.iterrows():
+    value = Vacancy(id=vacancy['id'],
+                    name=vacancy['name'],
+                    region_id=vacancy['region_id'],
+                    source_id=vacancy['source_id'],
+                    create_date=datetime.strptime(vacancy['create_date'], "%Y-%m-%dT%H:%M:%S%z"))
+    db.session.add(value)
+
+db.session.commit()
+
+
+classified_vacancies = pd.read_csv('data/classified_vacancies.csv')
+
+for index, vacancy in classified_vacancies.iterrows():
+
+    labels = vacancy['labels'].split(',')
+    for label in list(map(str.strip, labels)):
+        value = ClassificatedVacancy(
+            vacancy_id=vacancy['vacancy_id'],
+            profstandard_id=label,
+            probability=vacancy['p' + str(label)]
+        )
+        db.session.add(value)
 
 db.session.commit()
