9c7ea32be6bf4570354dc59317f297dc707078f0
master8 <master8.michael@gmail.com>
2018-12-10 19:03:15 +0500

    fixes

--- app.py
+++ app.py
@@ -5,7 +5,6 @@ from flask_sqlalchemy import SQLAlchemy
 import seaborn as sns
 
 import numpy as np
-import os
 from datetime import datetime
 
 from config import Config
@@ -18,11 +17,6 @@ migrate = Migrate(app, db)
 from models import Profstandard, Source, Region, Vacancy, ClassificatedVacancy
 
 
-# @app.route('/')
-# def hello_world():
-#     user = {'username': 'Michael'}
-#     return render_template('index.html', title="Hello", user=user)
-
 @app.route('/')
 def home():
     professions = Profstandard.query.all()
@@ -31,44 +25,42 @@ def home():
     return render_template('index.html', title='home', professions=professions, regions=regions, sources=sources)
 
 
-@app.route('/analyze')
-def analyze():
-    pr = Profstandard.query.get(14)
-    return 'analyze results ' + str(request.args.getlist('prof')) + ' ' + pr.name
+@app.route('/profession')
+def profession():
+    return 'profession ' + request.args['id']
+
 
 @app.route('/search')
 def search():
     reg_id = request.args.get('region')
-    region = Region.query.filter_by(id= reg_id).first().name
+    region = Region.query.get(reg_id).name
 
-    source_id =request.args.get('source')
-    source = Source.query.filter_by(id= source_id).first().name
+    source_id = request.args.get('source')
+    source = Source.query.get(source_id).name
 
     sdate = request.args.get('sdate')
     edate = request.args.get('edate')
-    dt_sdate = datetime.strptime(sdate,"%Y-%m-%d")
-    dt_edate = datetime.strptime(edate,"%Y-%m-%d")
+    dt_sdate = datetime.strptime(sdate, "%Y-%m-%d")
+    dt_edate = datetime.strptime(edate, "%Y-%m-%d")
 
-    period = 'from: ' + str(sdate)+' to: '+str(edate) #месяц - день - год
+    period = 'from: ' + str(sdate) + ' to: ' + str(edate)  # месяц - день - год
 
     prof_id_list = request.args.getlist('prof')
 
-
     professions = []
-
 
     for prof_id in prof_id_list:
         count = 0
 
-        class_vacancy = ClassificatedVacancy.query.filter_by(profstandard_id = prof_id)
+        class_vacancy = ClassificatedVacancy.query.filter_by(profstandard_id=prof_id)
         for sample in class_vacancy:
-            vacancy = Vacancy.query.filter_by(id=sample.vacancy_id)\
-                .filter(Vacancy.create_date <= dt_edate)\
-                .filter(Vacancy.create_date >= dt_sdate)\
-                .filter_by(region_id = reg_id)\
-                .filter_by(source_id = source_id)
+            vacancy = Vacancy.query.filter_by(id=sample.vacancy_id) \
+                .filter(Vacancy.create_date <= dt_edate) \
+                .filter(Vacancy.create_date >= dt_sdate) \
+                .filter_by(region_id=reg_id) \
+                .filter_by(source_id=source_id)
             for i in vacancy:
-                count +=1
+                count += 1
 
         prof_dict = {}
         prof_dict = {
@@ -79,8 +71,7 @@ def search():
         }
         professions.append(prof_dict)
 
-
-    if professions == []:
+    if not professions:
         professions = [{
             'profstandard_id': 0,
             'code': '',
@@ -89,7 +80,7 @@ def search():
         }]
 
     diagram_link = plot_search(professions)
-    
+
     return render_template('results.html',
                            title='results',
                            region=region,
@@ -97,7 +88,6 @@ def search():
                            period=period,
                            professions=professions,
                            diagram_link=diagram_link)
-
 
 
 def plot_search(professions):
@@ -114,11 +104,12 @@ def plot_search(professions):
     diagram.clear()
     diagram = sns.barplot(x=x, y=y)
     dia = diagram.get_figure()
-    
-    dia.savefig('./static/diagram/test_diagram.svg')   
+
+    dia.savefig('./static/diagram/test_diagram.svg')
     diagram_link = '../static/diagram/test_diagram.svg'
 
     return diagram_link
+
 
 @app.after_request
 def add_header(response):
@@ -128,4 +119,4 @@ def add_header(response):
 
 
 if __name__ == '__main__':
-    app.run(debug = True)
+    app.run(debug=True)
