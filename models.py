a58856d0009fea7ce798a736bac65ee7aeeea17b
omega1996 <35338204+omega1996@users.noreply.github.com>
2018-12-13 18:17:23 +0500

    added models

--- models.py
+++ models.py
@@ -2,6 +2,9 @@ from app import db
 
 
 class Region(db.Model):
+    '''
+
+    '''
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(64), index=True, unique=True)
 
@@ -12,97 +15,154 @@ class Region(db.Model):
 
 
 class Vacancy(db.Model):
+    '''
+
+    '''
     id = db.Column(db.Integer, primary_key=True)
     region_id = db.Column(db.Integer, db.ForeignKey('region.id'))
     source_id = db.Column(db.Integer, db.ForeignKey('source.id'))
-    name = db.Column(db.String(64))
+    name = db.Column(db.String(128))
     create_date = db.Column(db.DateTime, index=True)
 
-    # vacancy_part = db.relationship('VacancyPart', backref='vacancy', lazy='dynamic')
+    vacancy_part = db.relationship('VacancyPart', backref='vacancy', lazy='dynamic')
+    classification = db.relationship('ClassifiedVacancy', backref='vacancy')
 
     def __repr__(self):
         return '<Vacancy {}>'.format(self.name)
 
 
 class ClassifiedVacancy(db.Model):
+    '''
+
+    '''
     id = db.Column(db.Integer, primary_key=True)
     vacancy_id = db.Column(db.Integer, db.ForeignKey('vacancy.id'))
     profstandard_id = db.Column(db.Integer, db.ForeignKey('profstandard.id'))
     probability = db.Column(db.Float)
 
     def __repr__(self):
-        return '<ClassifiedVacancy {}>'.format(self.name)
+        return '<ClassifiedVacancy {}>'.format(self.vacancy_id)
 
 
-# class VacancyPart(db.Model):
-#     id = db.Column(db.Integer, primary_key=True)
-#     vac_id = db.Column(db.Integer, db.ForeignKey('vacancy.id'))
-#     competence = db.Column(db.Integer, db.ForeignKey('competence.id'))
-#     similarity = db.Column(db.Float)
-#     text = db.Column(db.Text) #? длина части вакансии или формат
-#     requirements_id = db.Column(db.Integer, db.ForeignKey('requirements.id'))
+class VacancyPart(db.Model):
+    '''
 
-#     competences = db.relationship('Competence', backref ='vac_part', lazy ='dynamic')
+    '''
+    id = db.Column(db.Integer, primary_key=True)
+    vac_id = db.Column(db.Integer, db.ForeignKey('vacancy.id'))
+    text = db.Column(db.Text)
+    type_id = db.Column(db.Integer, db.ForeignKey('vacancyparttype.id'))
 
-#     def __repr__(self):
-#         return '<VacancyPart {}>'.format(self.text)
+    matching = db.relationship('MatchPart', backref ='vacancy_part', lazy ='dynamic')
 
-# class Requirements(db.Model):
-#     id = db.Column(db.Integer, primary_key=True)
-#     name = db.Column(db.String(64), index=True, unique=True)
+    def __repr__(self):
+        return '<VacancyPart {}>'.format(self.text)
 
-#     vacancies = db.relationship('VacancyPart', backref ='requirement', lazy ='dynamic') #хз так или нет
+class VacancyPartType(db.Model):
+    '''
 
-#     def __repr__(self):
-#         return '<Requirements {}>'.format(self.name)
+    '''
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), index=True, unique=True)
 
-# class Competence(db.Model):
-#     id = db.Column(db.Integer, primary_key=True)
-#     name = db.Column(db.String(128), index=True)
-#     comp_type = db.Column(db.String(64), index=True)
-#     function_id = db.Column(db.Integer, db.ForeignKey('function.id'))
+    vacancy_parts = db.relationship('VacancyPart', backref ='type', lazy ='dynamic') #хз так или нет
 
-#     vacancy_parts = db.relationship('VacancyPart', backref='competence', lazy='dynamic')
+    def __repr__(self):
+        return '<VacancyPartType {}>'.format(self.name)
 
-#     def __repr__(self):
-#         return '<Competence {}>'.format(self.name)
+class ProfstandardPart(db.Model):
+    '''
+
+    '''
+    id = db.Column(db.Integer, primary_key=True)
+    text = db.Column(db.Text)
+    part_type_id = db.Column(db.Integer, db.ForeignKey('proftandardparttype.id'))
+    function_id = db.Column(db.Integer, db.ForeignKey('function.id'))
+
+    matched_parts = db.relationship('MatchPart', backref='competence', lazy='dynamic')
+
+    def __repr__(self):
+        return '<Competence {}>'.format(self.text)
 
 
-# class Function(db.Model):
-#     id = db.Column(db.Integer, primary_key=True)
-#     name = db.Column(db.String(128), index=True, unique=True)
-#     general_id = db.Column(db.Integer, db.ForeignKey('general.id'))
+class MatchPart(db.Model):
+    '''
 
-#     competences = db.relationship('Competence', backref='function', lazy='dynamic')
+    '''
+    id = db.Column(db.Integer, primary_key=True)
+    vacancy_part_id = db.Column(db.Integer, db.ForeignKey('vacancypart.id'))
+    profstandard_part_id = db.Column(db.Integer, db.ForeignKey('profstandardpart.id'))
+    similarity = db.Column(db.Float)
+    enriched_text = db.Column(db.Text)
 
-#     def __repr__(self):
-#         return '<Function {}>'.format(self.name)
+    def __repr__(self):
+        return '<MatchPart {}>'.format(self.enriched_text)
 
-# class General(db.Model):
-#     id = db.Column(db.Integer, primary_key=True)
-#     name = db.Column(db.String(128), index=True, unique=True)
-#     stand_code = db.Column(db.Integer, db.ForeignKey('profstandard.id'))
 
-#     functions = db.relationship('Function', backref='general_function', lazy='dynamic')
+class ProfstandardPartType(db.Model):
+    '''
 
-#     def __repr__(self):
-#         return '<General {}>'.format(self.name)
+    '''
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), index=True, unique=True)
+
+    vacancy_parts = db.relationship('ProfstandardPart', backref ='part_type', lazy ='dynamic') #хз так или нет
+
+    def __repr__(self):
+        return '<ProfstandardPartType {}>'.format(self.name)
+
+
+class Function(db.Model):
+    '''
+
+    '''
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(128), index=True, unique=True)
+    code = db.Column(db.String(64))
+    general_function_id = db.Column(db.Integer, db.ForeignKey('generalfunction.id'))
+    qualification_level = db.Column(db.Integer)
+
+    parts = db.relationship('ProfstandardPart', backref='function', lazy='dynamic')
+
+    def __repr__(self):
+        return '<Function {}>'.format(self.name)
+
+class GeneralFunction(db.Model):
+    '''
+
+    '''
+    id = db.Column(db.Integer, primary_key=True)
+    code = db.Column(db.String(64))
+    profstandard_id = db.Column(db.Integer, db.ForeignKey('profstandard.id'))
+    name = db.Column(db.String(128), index=True, unique=True)
+    qualification_level = db.Column(db.Integer)
+
+
+    functions = db.relationship('Function', backref='general_function', lazy='dynamic')
+
+    def __repr__(self):
+        return '<GeneralFunction {}>'.format(self.name)
 
 
 class Profstandard(db.Model):
+    '''
+    '''
     id = db.Column(db.Integer, primary_key=True)
-    code = db.Column(db.Float, unique=True)
+    code = db.Column(db.String(64), unique=True)
     name = db.Column(db.String(128), index=True, unique=True)
     is_support = db.Column(db.Boolean)
 
-    # generals = db.relationship('General', backref='profstandard', lazy='dynamic')
-    vacancies = db.relationship('ClassifiedVacancy', backref ='profstandard', lazy ='dynamic')
+    general_funcions = db.relationship('GeneralFunction', backref='profstandard', lazy='dynamic')
+    classified_vacancies = db.relationship('ClassifiedVacancy', backref ='profstandard', lazy ='dynamic')
 
     def __repr__(self):
         return '<Profstandard {}>'.format(self.name)
 
 
 class Source(db.Model):
+    '''
+
+    '''
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(64), index=True, unique=True)
     is_support = db.Column(db.Boolean)
