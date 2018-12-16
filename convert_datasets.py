4d3e157f0e242c30e125e13aed9c96a475a66888
master8 <master8.michael@gmail.com>
2018-12-16 15:41:37 +0500

    fix script

--- convert_datasets.py
+++ convert_datasets.py
@@ -17,14 +17,27 @@ import numpy as np
 # vr['ind'] = v.ind
 # t.to_csv('data/t_vacancies_parts.csv', index=False)
 # vr.to_csv('data/u_vacancies_parts_t.csv', index=False)
-
+from app import db
+from models import MatchPart
 
 v = pd.read_csv('data/u_vacancies_parts_t.csv')
 p = pd.read_csv('data/u_standards_parts_t.csv')
 s = pd.DataFrame()
 co = pd.read_csv('data/sim.csv')
+co = co[co.id_profstandard_part.notna()]
+co.id_profstandard_part = co.id_profstandard_part.astype('int')
 
-s['profstandard_part_id'] = co.id_profstandard_part.apply(lambda x: np.array(p[p.ind == x].id)[0])
+
+def getProfstandard_id(x):
+    temp = np.array(p[p.ind == x].id)
+    if len(temp) > 0:
+        return temp[0]
+    else:
+        return None
+
+
+s['profstandard_part_id'] = co.id_profstandard_part.apply(getProfstandard_id)
+s = s[s.profstandard_part_id.notna()]
 s['vacancy_part_id'] = co.id_vacancy_part.apply(lambda x: np.array(v[v.ind == x].id)[0])
 s['similarity'] = co.sc
 s['enriched_text'] = co.prof_text
@@ -35,3 +48,13 @@ t['similarity'] = s.similarity
 t['enriched_text'] = s.enriched_text
 t.to_csv('data/t_match_parts.csv', index=False)
 
+match_parts = pd.read_csv('data/t_match_parts.csv')
+
+for index, part in match_parts.iterrows():
+    value = MatchPart(vacancy_part_id=part['vacancy_part_id'],
+                      profstandard_part_id=part['profstandard_part_id'],
+                      similarity=part['similarity'],
+                      enriched_text=part['enriched_text'])
+    db.session.add(value)
+
+db.session.commit()
