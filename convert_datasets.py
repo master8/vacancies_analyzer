e87a36718c143e72de89334570371970807c3565
master8 <master8.michael@gmail.com>
2018-12-17 02:57:26 +0500

    add data

--- convert_datasets.py
+++ convert_datasets.py
@@ -20,41 +20,44 @@ import numpy as np
 from app import db
 from models import MatchPart
 
-v = pd.read_csv('data/u_vacancies_parts_t.csv')
-p = pd.read_csv('data/u_standards_parts_t.csv')
-s = pd.DataFrame()
-co = pd.read_csv('data/sim.csv')
-co = co[co.id_profstandard_part.notna()]
-co.id_profstandard_part = co.id_profstandard_part.astype('int')
+
+for i in range(12, 23):
+
+    v = pd.read_csv('../data/u_vacancies_parts_t.csv')
+    p = pd.read_csv('../data/u_standards_parts_t.csv')
+    s = pd.DataFrame()
+    co = pd.read_csv('../sim/similar_samples_W2V_own_part_' + str(i) + '.csv')
+    co = co[co.id_profstandard_part.notna()]
+    co.id_profstandard_part = co.id_profstandard_part.astype('int')
 
 
-def getProfstandard_id(x):
-    temp = np.array(p[p.ind == x].id)
-    if len(temp) > 0:
-        return temp[0]
-    else:
-        return None
+    def getProfstandard_id(x):
+        temp = np.array(p[p.ind == x].id)
+        if len(temp) > 0:
+            return temp[0]
+        else:
+            return None
 
 
-s['profstandard_part_id'] = co.id_profstandard_part.apply(getProfstandard_id)
-s = s[s.profstandard_part_id.notna()]
-s['vacancy_part_id'] = co.id_vacancy_part.apply(lambda x: np.array(v[v.ind == x].id)[0])
-s['similarity'] = co.sc
-s['enriched_text'] = co.prof_text
-t = pd.DataFrame()
-t['vacancy_part_id'] = s.vacancy_part_id
-t['profstandard_part_id'] = s.profstandard_part_id
-t['similarity'] = s.similarity
-t['enriched_text'] = s.enriched_text
-t.to_csv('data/t_match_parts.csv', index=False)
+    s['profstandard_part_id'] = co.id_profstandard_part.apply(getProfstandard_id)
+    s = s[s.profstandard_part_id.notna()]
+    s['vacancy_part_id'] = co.id_vacancy_part.apply(lambda x: np.array(v[v.ind == x].id)[0])
+    s['similarity'] = co.sc
+    s['enriched_text'] = co.prof_text
+    t = pd.DataFrame()
+    t['vacancy_part_id'] = s.vacancy_part_id
+    t['profstandard_part_id'] = s.profstandard_part_id
+    t['similarity'] = s.similarity
+    t['enriched_text'] = s.enriched_text
+    t.to_csv('../data/t_match_parts.csv', index=False)
 
-match_parts = pd.read_csv('data/t_match_parts.csv')
+    match_parts = pd.read_csv('../data/t_match_parts.csv')
 
-for index, part in match_parts.iterrows():
-    value = MatchPart(vacancy_part_id=part['vacancy_part_id'],
-                      profstandard_part_id=part['profstandard_part_id'],
-                      similarity=part['similarity'],
-                      enriched_text=part['enriched_text'])
-    db.session.add(value)
+    for index, part in match_parts.iterrows():
+        value = MatchPart(vacancy_part_id=part['vacancy_part_id'],
+                          profstandard_part_id=part['profstandard_part_id'],
+                          similarity=part['similarity'],
+                          enriched_text=part['enriched_text'])
+        db.session.add(value)
 
-db.session.commit()
+    db.session.commit()
