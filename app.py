4e53642f5b753d2d194515fa5620def64734d058
starminalush <nastja1905@mail.ru>
2019-03-02 18:46:38 +0500

    add university program

--- app.py
+++ app.py
@@ -1,4 +1,8 @@
 from flask import Flask, request
+import matplotlib
+
+matplotlib.use('agg')
+import matplotlib.pyplot as plt
 from flask import render_template
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
@@ -13,217 +17,151 @@ from config import Config
 app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
+db.create_all()
 migrate = Migrate(app, db)
 
-from models import Profstandard, Source, Region, Vacancy, ClassifiedVacancy
-from models import GeneralFunction, Function, ProfstandardPart, MatchPart, VacancyPart
+from models import University
 
 
 @app.route('/')
 def home():
-    professions = Profstandard.query.all()
-    regions = Region.query.all()
-    sources = Source.query.all()
-    return render_template('index.html', title='home', professions=professions, regions=regions, sources=sources)
+    universities = University.query.all()
+    return render_template('index.html', title='home', universities=universities)
 
 
-@app.route('/search')
-def search():
-    reg_id = request.args.get('region')
-    region = Region.query.get(reg_id)
-
-    source_id = request.args.get('source')
-    source = Source.query.get(source_id)
-
-    sdate = request.args.get('sdate')
-    edate = request.args.get('edate')
-    dt_sdate = datetime.strptime(sdate, "%Y-%m-%d")
-    dt_edate = datetime.strptime(edate, "%Y-%m-%d")
-
-    period = 'from: ' + str(sdate) + ' to: ' + str(edate)  # месяц - день - год
-
-    prof_id_list = request.args.getlist('prof')
-
-    professions = []
-
-    for prof_id in prof_id_list:
-        vacancy = Vacancy.query \
-            .filter(ClassifiedVacancy.profstandard_id == prof_id) \
-            .filter(Vacancy.id == ClassifiedVacancy.vacancy_id) \
-            .filter(Vacancy.create_date <= dt_edate) \
-            .filter(Vacancy.create_date >= dt_sdate) \
-            .filter_by(region_id=reg_id) \
-            .filter_by(source_id=source_id)
-
-        prof_dict = {
-            'profstandard_id': prof_id,
-            'code': Profstandard.query.get(prof_id).code,
-            'name': Profstandard.query.get(prof_id).name,
-            'count': vacancy.count()
-        }
-        professions.append(prof_dict)
-
-    if not professions:
-        professions = [{
-            'profstandard_id': 0,
-            'code': '',
-            'name': 'Профессия не выбрана',
-            'count': 0
-        }]
-
-    diagram_link = plot_search(professions)
-
-    return render_template('results.html',
-                           title='results',
-                           region=region,
-                           source=source,
-                           period=period,
-                           professions=professions,
-                           diagram_link=diagram_link,
-                           sdate=request.args['sdate'],
-                           edate=request.args['edate'])
-
-
-@app.route('/profession')
+@app.route('/edprogram')
 def profession():
     reg_id = request.args.get('region')
-
-    source_id = request.args.get('source')
-    prof_id = request.args.get('id')
-    sdate = request.args.get('sdate')
-    edate = request.args.get('edate')
-    dt_sdate = datetime.strptime(sdate, "%Y-%m-%d")
-    dt_edate = datetime.strptime(edate, "%Y-%m-%d")
-
-    query = db.session.query(Vacancy, ClassifiedVacancy) \
-        .filter(ClassifiedVacancy.profstandard_id == prof_id) \
-        .filter(Vacancy.id == ClassifiedVacancy.vacancy_id) \
-        .filter(Vacancy.create_date <= dt_edate) \
-        .filter(Vacancy.create_date >= dt_sdate) \
-        .filter_by(region_id=reg_id) \
-        .filter_by(source_id=source_id) \
-        .order_by(ClassifiedVacancy.probability)
-
-    best_vacancies = []
-    worst_vacancies = []
-
-    for vacancy, classified_vacancy in query[:10]:
-        worst_vacancy = {
-            'id': vacancy.id,
-            'name': vacancy.name,
-            'probability': str(classified_vacancy.probability)[:6]
-        }
-        worst_vacancies.append(worst_vacancy)
-
-    for vacancy, classified_vacancy in reversed(query[-10:]):
-        best_vacancy = {
-            'id': vacancy.id,
-            'name': vacancy.name,
-            'probability': str(classified_vacancy.probability)[:6]
-        }
-        best_vacancies.append(best_vacancy)
-
-    query = db.session.query(MatchPart, VacancyPart) \
-        .filter(MatchPart.vacancy_part_id == VacancyPart.id) \
-        .filter(VacancyPart.vacancy_id == Vacancy.id) \
-        .filter(ClassifiedVacancy.profstandard_id == prof_id) \
-        .filter(Vacancy.id == ClassifiedVacancy.vacancy_id) \
-        .filter(Vacancy.create_date <= dt_edate) \
-        .filter(Vacancy.create_date >= dt_sdate) \
-        .filter(Vacancy.region_id == reg_id) \
-        .filter(Vacancy.source_id == source_id)
-
-    matched_parts = defaultdict(list)
-
-    for match_part, vacancy_part in query:
-        matched_parts[match_part.profstandard_part_id].append({
-            'similarity': match_part.similarity,
-            'vacancy_part': vacancy_part.text
-        })
-
-    general_functions = general_function_tree(prof_id, matched_parts)
+    # query = db.session.query(Vacancy, ClassifiedVacancy) \
+    #     .filter(ClassifiedVacancy.profstandard_id == prof_id) \
+    #     .filter(Vacancy.id == ClassifiedVacancy.vacancy_id) \
+    #     .filter(Vacancy.create_date <= dt_edate) \
+    #     .filter(Vacancy.create_date >= dt_sdate) \
+    #     .filter_by(region_id=reg_id) \
+    #     .filter_by(source_id=source_id) \
+    #     .order_by(ClassifiedVacancy.probability)
+    #
+    # best_vacancies = []
+    # worst_vacancies = []
+    #
+    # for vacancy, classified_vacancy in query[:10]:
+    #     worst_vacancy = {
+    #         'id': vacancy.id,
+    #         'name': vacancy.name,
+    #         'probability': str(classified_vacancy.probability)[:6]
+    #     }
+    #     worst_vacancies.append(worst_vacancy)
+    #
+    # for vacancy, classified_vacancy in reversed(query[-10:]):
+    #     best_vacancy = {
+    #         'id': vacancy.id,
+    #         'name': vacancy.name,
+    #         'probability': str(classified_vacancy.probability)[:6]
+    #     }
+    #     best_vacancies.append(best_vacancy)
+    #
+    # query = db.session.query(MatchPart, VacancyPart) \
+    #     .filter(MatchPart.vacancy_part_id == VacancyPart.id) \
+    #     .filter(VacancyPart.vacancy_id == Vacancy.id) \
+    #     .filter(ClassifiedVacancy.profstandard_id == prof_id) \
+    #     .filter(Vacancy.id == ClassifiedVacancy.vacancy_id) \
+    #     .filter(Vacancy.create_date <= dt_edate) \
+    #     .filter(Vacancy.create_date >= dt_sdate) \
+    #     .filter(Vacancy.region_id == reg_id) \
+    #     .filter(Vacancy.source_id == source_id)
+    #
+    # matched_parts = defaultdict(list)
+    #
+    # for match_part, vacancy_part in query:
+    #     matched_parts[match_part.profstandard_part_id].append({
+    #         'similarity': match_part.similarity,
+    #         'vacancy_part': vacancy_part.text
+    #     })
+    #
+    # general_functions = general_function_tree(prof_id, matched_parts)
 
     return render_template('profession.html',
                            title='profession',
-                           best_vacancies=best_vacancies,
-                           worst_vacancies=worst_vacancies,
-                           profession=Profstandard.query.get(prof_id).name,
-                           general_functions=general_functions)
+                           education_program=["09.03.01. Информатика и вычислительная техника"],
+                           disciplines = ["Программирование", "Базы данных", "Алгоритмы и анализ сложности", "Анализ данных"],
+                           zyn = ["ОПК-1", "ПК-1", "ПК-2"],
+                           parts = ["Блок-схема алгоритма", "Введение в язык программирования СИ"],
+                           themes = ["Блок-схема алгоритма", "Массивы -одномерные. Алгоритмы работы с массивами"])
 
 
-def general_function_tree(prof_id, matched_parts):
-    tree = []
-    query = GeneralFunction.query.filter_by(profstandard_id=prof_id)
-    for each in query:
-        functions, function_weight = function_branch(each.id, matched_parts)
-        general_function_branch = {
-            'weight': function_weight,
-            'name': each.name,
-            'functions': functions
-        }
-        tree.append(general_function_branch)
-    return tree
-
-
-def function_branch(general_id, matched_parts):
-    branch = []
-    weight = 0
-    query = Function.query.filter_by(general_function_id=general_id)
-    for each in query:
-        parts, vacancy_weight = parts_vacancies_leafs(each.id, matched_parts)
-        function_parts_branch = {
-            'weight': vacancy_weight,
-            'name': each.name,
-            'parts': parts
-        }
-        branch.append(function_parts_branch)
-        weight += vacancy_weight
-    return branch, weight
-
-
-def parts_vacancies_leafs(function_id, matched_parts):
-    leaf = []
-    query = ProfstandardPart.query.filter_by(function_id=function_id)
-    weight = 0
-    for each in query:
-        vacancy_parts = matched_parts[each.id]
-        parts_weight = len(vacancy_parts)
-        leaf_parts = {
-            'weight': parts_weight,
-            'standard_part': each.text,
-            'vacancy_parts': vacancy_parts
-        }
-        leaf.append(leaf_parts)
-        weight += parts_weight
-
-    return leaf, weight
-
-
-@app.route('/vacancy')
-def vacancy():
-    return 'vacancy ' + str(request.args['id'])
-
-
-def plot_search(professions):
-    t = []
-    num = []
-
-    for i in professions:
-        t.append(str(i['code']))
-        num.append(i['count'])
-    x = np.array(t)
-    y = np.array(num)
-
-    diagram = sns.barplot(x=x, y=y)
-    diagram.clear()
-    diagram = sns.barplot(x=x, y=y)
-    dia = diagram.get_figure()
-
-    dia.savefig('./static/diagram/test_diagram.svg')
-    diagram_link = '../static/diagram/test_diagram.svg'
-
-    return diagram_link
-
+# def general_function_tree(prof_id, matched_parts):
+#     tree = []
+#     query = GeneralFunction.query.filter_by(profstandard_id=prof_id)
+#     for each in query:
+#         functions, function_weight = function_branch(each.id, matched_parts)
+#         general_function_branch = {
+#             'weight': function_weight,
+#             'name': each.name,
+#             'functions': functions
+#         }
+#         tree.append(general_function_branch)
+#     return tree
+#
+#
+# def function_branch(general_id, matched_parts):
+#     branch = []
+#     weight = 0
+#     query = Function.query.filter_by(general_function_id=general_id)
+#     for each in query:
+#         parts, vacancy_weight = parts_vacancies_leafs(each.id, matched_parts)
+#         function_parts_branch = {
+#             'weight': vacancy_weight,
+#             'name': each.name,
+#             'parts': parts
+#         }
+#         branch.append(function_parts_branch)
+#         weight += vacancy_weight
+#     return branch, weight
+#
+#
+# def parts_vacancies_leafs(function_id, matched_parts):
+#     leaf = []
+#     query = ProfstandardPart.query.filter_by(function_id=function_id)
+#     weight = 0
+#     for each in query:
+#         vacancy_parts = matched_parts[each.id]
+#         parts_weight = len(vacancy_parts)
+#         leaf_parts = {
+#             'weight': parts_weight,
+#             'standard_part': each.text,
+#             'vacancy_parts': vacancy_parts
+#         }
+#         leaf.append(leaf_parts)
+#         weight += parts_weight
+#
+#     return leaf, weight
+#
+#
+# @app.route('/vacancy')
+# def vacancy():
+#     return 'vacancy ' + str(request.args['id'])
+#
+#
+# def plot_search(professions):
+#     t = []
+#     num = []
+#
+#     for i in professions:
+#         t.append(str(i['code']))
+#         num.append(i['count'])
+#     x = np.array(t)
+#     y = np.array(num)
+#
+#     diagram = sns.barplot(x=x, y=y)
+#     diagram.clear()
+#     diagram = sns.barplot(x=x, y=y)
+#     dia = diagram.get_figure()
+#
+#     dia.savefig('./static/diagram/test_diagram.svg')
+#     diagram_link = '../static/diagram/test_diagram.svg'
+#
+#     return diagram_link
+#
 
 @app.after_request
 def add_header(response):
--- models.py
+++ models.py
@@ -1,165 +1,160 @@
 from app import db
 
 
-class Profstandard(db.Model):
+class University(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
-    code = db.Column(db.String(64), unique=True)
     name = db.Column(db.String(128), index=True, unique=True)
-    is_support = db.Column(db.Boolean)
-
-    general_funcions = db.relationship('GeneralFunction', backref='profstandard', lazy='dynamic')
-    classified_vacancies = db.relationship('ClassifiedVacancy', backref='profstandard', lazy='dynamic')
 
     def __repr__(self):
-        return '<Profstandard {}>'.format(self.name)
-
-
-class GeneralFunction(db.Model):
-
-    id = db.Column(db.Integer, primary_key=True)
-    code = db.Column(db.String(64))
-    profstandard_id = db.Column(db.Integer, db.ForeignKey('profstandard.id'))
-    name = db.Column(db.String(128), index=True, unique=True)
-    qualification_level = db.Column(db.Integer)
-
-    functions = db.relationship('Function', backref='general_function', lazy='dynamic')
-
-    def __repr__(self):
-        return '<GeneralFunction {}>'.format(self.name)
-
-
-class Function(db.Model):
-
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(128), index=True)
-    code = db.Column(db.String(64))
-    general_function_id = db.Column(db.Integer, db.ForeignKey('general_function.id'))
-    qualification_level = db.Column(db.Integer)
-
-    parts = db.relationship('ProfstandardPart', backref='function', lazy='dynamic')
-
-    def __repr__(self):
-        return '<Function {}>'.format(self.name)
-
-
-class ProfstandardPartType(db.Model):
-
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(64), index=True, unique=True)
-
-    vacancy_parts = db.relationship('ProfstandardPart', backref='part_type', lazy='dynamic')  # хз так или нет
-
-    def __repr__(self):
-        return '<ProfstandardPartType {}>'.format(self.name)
-
-
-class ProfstandardPart(db.Model):
-
-    id = db.Column(db.Integer, primary_key=True)
-    text = db.Column(db.Text)
-    part_type_id = db.Column(db.Integer, db.ForeignKey('profstandard_part_type.id'))
-    function_id = db.Column(db.Integer, db.ForeignKey('function.id'))
-
-    matched_parts = db.relationship('MatchPart', backref='competence', lazy='dynamic')
-
-    def __repr__(self):
-        return '<Competence {}>'.format(self.text)
-
-
-class Region(db.Model):
-
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(64), index=True, unique=True)
-
-    vacancies = db.relationship('Vacancy', backref='region', lazy='dynamic')
-
-    def __repr__(self):
-        return '<Region {}>'.format(self.name)
-
-
-class Source(db.Model):
-
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(64), index=True, unique=True)
-    is_support = db.Column(db.Boolean)
-
-    vacancies = db.relationship('Vacancy', backref='source', lazy='dynamic')
-
-    def __repr__(self):
-        return '<Source {}>'.format(self.name)
-
-
-class ClassifiedVacancy(db.Model):
-
-    id = db.Column(db.Integer, primary_key=True)
-    vacancy_id = db.Column(db.Integer, db.ForeignKey('vacancy.id'))
-    profstandard_id = db.Column(db.Integer, db.ForeignKey('profstandard.id'))
-    probability = db.Column(db.Float)
-
-    def __repr__(self):
-        return '<ClassifiedVacancy {}>'.format(self.vacancy_id)
-
-
-class Vacancy(db.Model):
-
-    id = db.Column(db.Integer, primary_key=True)
-    region_id = db.Column(db.Integer, db.ForeignKey('region.id'))
-    source_id = db.Column(db.Integer, db.ForeignKey('source.id'))
-    name = db.Column(db.String(128))
-    create_date = db.Column(db.DateTime, index=True)
-
-    vacancy_part = db.relationship('VacancyPart', backref='vacancy', lazy='dynamic')
-    classification = db.relationship('ClassifiedVacancy', backref='vacancy', lazy='dynamic')
-
-    def __repr__(self):
-        return '<Vacancy {}>'.format(self.name)
-
-
-class VacancyPart(db.Model):
-
-    id = db.Column(db.Integer, primary_key=True)
-    vacancy_id = db.Column(db.Integer, db.ForeignKey('vacancy.id'))
-    text = db.Column(db.Text)
-    type_id = db.Column(db.Integer, db.ForeignKey('vacancy_part_type.id'))
-
-    matching = db.relationship('MatchPart', backref='vacancy_part', lazy='dynamic')
-
-    def __repr__(self):
-        return '<VacancyPart {}>'.format(self.text)
-
-
-class VacancyPartType(db.Model):
-
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(64), index=True, unique=True)
-
-    vacancy_parts = db.relationship('VacancyPart', backref='type', lazy='dynamic')  # хз так или нет
-
-    def __repr__(self):
-        return '<VacancyPartType {}>'.format(self.name)
-
-
-class MatchPart(db.Model):
-
-    id = db.Column(db.Integer, primary_key=True)
-    vacancy_part_id = db.Column(db.Integer, db.ForeignKey('vacancy_part.id'))
-    profstandard_part_id = db.Column(db.Integer, db.ForeignKey('profstandard_part.id'))
-    similarity = db.Column(db.Float)
-    enriched_text = db.Column(db.Text)
-
-    def __repr__(self):
-        return '<MatchPart {}>'.format(self.enriched_text)
-
-
-
-
-
-
-
-
-
-
-
-
-
+        return '<University {}>'.format(self.name)
+
+
+# class GeneralFunction(db.Model):
+#
+#     id = db.Column(db.Integer, primary_key=True)
+#     code = db.Column(db.String(64))
+#     profstandard_id = db.Column(db.Integer, db.ForeignKey('profstandard.id'))
+#     name = db.Column(db.String(128), index=True, unique=True)
+#     qualification_level = db.Column(db.Integer)
+#
+#     functions = db.relationship('Function', backref='general_function', lazy='dynamic')
+#
+#     def __repr__(self):
+#         return '<GeneralFunction {}>'.format(self.name)
+#
+#
+# class Function(db.Model):
+#
+#     id = db.Column(db.Integer, primary_key=True)
+#     name = db.Column(db.String(128), index=True)
+#     code = db.Column(db.String(64))
+#     general_function_id = db.Column(db.Integer, db.ForeignKey('general_function.id'))
+#     qualification_level = db.Column(db.Integer)
+#
+#     parts = db.relationship('ProfstandardPart', backref='function', lazy='dynamic')
+#
+#     def __repr__(self):
+#         return '<Function {}>'.format(self.name)
+#
+#
+# class ProfstandardPartType(db.Model):
+#
+#     id = db.Column(db.Integer, primary_key=True)
+#     name = db.Column(db.String(64), index=True, unique=True)
+#
+#     vacancy_parts = db.relationship('ProfstandardPart', backref='part_type', lazy='dynamic')  # хз так или нет
+#
+#     def __repr__(self):
+#         return '<ProfstandardPartType {}>'.format(self.name)
+#
+#
+# class ProfstandardPart(db.Model):
+#
+#     id = db.Column(db.Integer, primary_key=True)
+#     text = db.Column(db.Text)
+#     part_type_id = db.Column(db.Integer, db.ForeignKey('profstandard_part_type.id'))
+#     function_id = db.Column(db.Integer, db.ForeignKey('function.id'))
+#
+#     matched_parts = db.relationship('MatchPart', backref='competence', lazy='dynamic')
+#
+#     def __repr__(self):
+#         return '<Competence {}>'.format(self.text)
+#
+#
+# class Region(db.Model):
+#
+#     id = db.Column(db.Integer, primary_key=True)
+#     name = db.Column(db.String(64), index=True, unique=True)
+#
+#     vacancies = db.relationship('Vacancy', backref='region', lazy='dynamic')
+#
+#     def __repr__(self):
+#         return '<Region {}>'.format(self.name)
+#
+#
+# class Source(db.Model):
+#
+#     id = db.Column(db.Integer, primary_key=True)
+#     name = db.Column(db.String(64), index=True, unique=True)
+#     is_support = db.Column(db.Boolean)
+#
+#     vacancies = db.relationship('Vacancy', backref='source', lazy='dynamic')
+#
+#     def __repr__(self):
+#         return '<Source {}>'.format(self.name)
+#
+#
+# class ClassifiedVacancy(db.Model):
+#
+#     id = db.Column(db.Integer, primary_key=True)
+#     vacancy_id = db.Column(db.Integer, db.ForeignKey('vacancy.id'))
+#     profstandard_id = db.Column(db.Integer, db.ForeignKey('profstandard.id'))
+#     probability = db.Column(db.Float)
+#
+#     def __repr__(self):
+#         return '<ClassifiedVacancy {}>'.format(self.vacancy_id)
+#
+#
+# class Vacancy(db.Model):
+#
+#     id = db.Column(db.Integer, primary_key=True)
+#     region_id = db.Column(db.Integer, db.ForeignKey('region.id'))
+#     source_id = db.Column(db.Integer, db.ForeignKey('source.id'))
+#     name = db.Column(db.String(128))
+#     create_date = db.Column(db.DateTime, index=True)
+#
+#     vacancy_part = db.relationship('VacancyPart', backref='vacancy', lazy='dynamic')
+#     classification = db.relationship('ClassifiedVacancy', backref='vacancy', lazy='dynamic')
+#
+#     def __repr__(self):
+#         return '<Vacancy {}>'.format(self.name)
+#
+#
+# class VacancyPart(db.Model):
+#
+#     id = db.Column(db.Integer, primary_key=True)
+#     vacancy_id = db.Column(db.Integer, db.ForeignKey('vacancy.id'))
+#     text = db.Column(db.Text)
+#     type_id = db.Column(db.Integer, db.ForeignKey('vacancy_part_type.id'))
+#
+#     matching = db.relationship('MatchPart', backref='vacancy_part', lazy='dynamic')
+#
+#     def __repr__(self):
+#         return '<VacancyPart {}>'.format(self.text)
+#
+#
+# class VacancyPartType(db.Model):
+#
+#     id = db.Column(db.Integer, primary_key=True)
+#     name = db.Column(db.String(64), index=True, unique=True)
+#
+#     vacancy_parts = db.relationship('VacancyPart', backref='type', lazy='dynamic')  # хз так или нет
+#
+#     def __repr__(self):
+#         return '<VacancyPartType {}>'.format(self.name)
+#
+#
+# class MatchPart(db.Model):
+#
+#     id = db.Column(db.Integer, primary_key=True)
+#     vacancy_part_id = db.Column(db.Integer, db.ForeignKey('vacancy_part.id'))
+#     profstandard_part_id = db.Column(db.Integer, db.ForeignKey('profstandard_part.id'))
+#     similarity = db.Column(db.Float)
+#     enriched_text = db.Column(db.Text)
+#
+#     def __repr__(self):
+#         return '<MatchPart {}>'.format(self.enriched_text)
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
