7072c4fe3c8a4aa6862de199fc9f6435219ba981
omega1996 <35338204+omega1996@users.noreply.github.com>
2019-03-13 21:48:07 +0500

    pobeda

--- app.py
+++ app.py
@@ -29,7 +29,7 @@ migrate = Migrate(app, db)
 morph = pymorphy2.MorphAnalyzer()
 
 from models import MatchPart, VacancyPart, ProfstandardPost, VacancyPartType, Profstandard, Source, Region, Vacancy, \
-    ClassifiedVacancy, University, EducationProgram
+    ClassifiedVacancy, University, EducationProgram, ProfstandardPart, GeneralFunction, Function
 
 from dto import Params, SelectedItems, Selected
 
@@ -368,6 +368,7 @@ def selected():
             competence.append([codes[i], names[i], standards])
         session['competence'] = competence
 
+        # print(session['competence'])
     if 'selected' in session:
         params = session['params']
         professions = []
@@ -471,9 +472,9 @@ def education_program(id_program):
     for comp in competences:
         full_comp.append(comp[1])
         pass
-
+    print(competences)
     # full_comp = ['управление проектами','разработка требований','Анаиз требований']
-
+    print(full_comp)
     zyn_df = pd.DataFrame(columns=['zyn_text'])
     all_zyn = list()
     all_id = list()
@@ -512,10 +513,13 @@ def education_program(id_program):
 
         def getsim(type):
             zyn_all = []
+            sum_sim = 0
             for item in range(len(gg_program[gg_program['type'] == type].full_text_match.tolist())):
+                sum_sim+=gg_program[gg_program['type'] == type].similarity.tolist()[item]
                 zyn_all.append((gg_program[gg_program['type'] == type].full_text_match.tolist()[item],
                                 gg_program[gg_program['type'] == type].similarity.tolist()[item],
                                gg_program[gg_program['type'] == type].zyn_index.tolist()[item]))
+            zyn_all.append(([],[],sum_sim))
             return zyn_all
 
 
@@ -524,6 +528,7 @@ def education_program(id_program):
         own_all = getall('own')
 
         know_tags = getsim('know')
+        print(know_tags)
         can_tags = getsim('can')
         own_tags = getsim('on')
 
